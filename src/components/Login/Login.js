0c9b87ae1c20bf0002f4ead3265d9b7eab37277e
Adela <adelangr822@gmail.com>
2022-06-23 14:46:25 +0800

    login ui added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,85 @@
-import React from 'react'
+import React from "react";
+
+import {
+  Button,
+  TextField,
+  Paper,
+  Box,
+  Grid,
+  Typography,
+
+} from "@mui/material";
+
 
 const Login = () => {
+ 
   return (
-    <div>
-   
-    </div>
-  )
-}
+      <Grid
+        container
+        component="main"
+        sx={{ height: "100vh" }}
+        data-testid="login"
+      >
+      
+      
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+        
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+            //   onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                // value={email}
+                // onChange={(e) => setEmail(e.target.value)}
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                // value={password}
+                // onChange={(e) => setPassword(e.target.value)}
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
 
-export default Login
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+  );
+};
+
+export default Login;
