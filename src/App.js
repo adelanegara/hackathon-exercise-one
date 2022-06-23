eb60dec99741e9372640dfeca628ba72515a9c5c
Adela <adelangr822@gmail.com>
2022-06-23 14:29:26 +0800

    json server

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 
 function App() {
   return (
-   <Router> <Routes>
-   <Route path="/login" element={<Home/>} />  
-   
+   <Router> 
+     <Routes>
+       <Route path="/" element={<Home/>} />  
+       <Route path="/login" element={<Login/>} />  
 
-   </Routes></Router>
+   </Routes>
+   </Router>
   );
 }
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 const Home = () => {
+    const [data, setData] = useState();
+    const fetchData = async () => {
+        const tradeList = await axios.get("http://localhost:3006/slot");
+        setData(tradeList.data);
+      };
+      useEffect(() => {
+        fetchData();
+      }, []);
   return (
-    <div>Home</div>
+    <div>    <h1>Book Parking </h1>
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Type</th>
+          <th scope="col">Date</th>
+          <th scope="col">Status</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data?.map((item, index) => (
+          <tr key={index}>
+            <th scope="row">{index + 1}</th>
+            <td>{item.type}</td>
+            <td>{item.date}</td>
+            <td>{item.status}</td>
+            <td className="d-flex flex-row">
+              <button
+                className="btn btn-sm btn-primary mr-1"
+              >
+                Book
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table></div>
   )
 }
 
