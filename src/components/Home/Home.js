c2b5e308785451f882dd27552d2112675ad19068
Adela <adelangr822@gmail.com>
2022-06-24 03:05:40 +0800

    deleted modal

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,35 +1,18 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
-import Box from "@mui/material/Box";
-import Modal from "@mui/material/Modal";
 import { Link } from "react-router-dom";
-import { Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
 
 const Home = () => {
   const navigate = useNavigate();
-  const [open, setOpen] = React.useState(false);
-  const handleClose = () => setOpen(false);
   const [data, setData] = useState();
   const username = localStorage.getItem("username");
   const role = localStorage.getItem("role");
 
   const fetchData = async () => {
-    const tradeList = await axios.get("http://localhost:3006/slot");
-    setData(tradeList.data);
+    const slotList = await axios.get("http://localhost:3006/slot");
+    setData(slotList.data);
   };
   useEffect(() => {
     fetchData();
@@ -89,29 +72,10 @@ const Home = () => {
               ))}
             </tbody>
           </table>
-          <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-          >
-            <Box sx={style}>
-              <div className="row">
-                <div className="col-md-6">
-                  <Form.Group controlId="dob">
-                    <Form.Label>Select Date</Form.Label>
-                    <Form.Control
-                      type="date"
-                      name="dob"
-                      placeholder="Date of Birth"
-                    />
-                  </Form.Group>
-                </div>
-              </div>
-              <button className="btn btn-secondary">Book</button>
-            </Box>
-          </Modal>
+    
+
         </div>
+        
       </div>
     </div>
   );
