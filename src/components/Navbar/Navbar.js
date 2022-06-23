d798f4d7b52ccd6acc543cee7d737a356ed68703
Adela <adelangr822@gmail.com>
2022-06-23 16:06:18 +0800

    fixed privateroutes.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,31 +1,42 @@
-import React  from "react";
+import React from "react";
 import { AppBar, Box, Toolbar, Typography, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Navbar = () => {
+  const navigate = useNavigate();
 
+  const onLogout = () => {
+    localStorage.clear();
+    toast.success("logout successfully");
+    navigate("/login");
+  };
   return (
     <div>
       <Box sx={{ flexGrow: 1 }} data-testid="navbar">
         <AppBar sx={{ bgcolor: "#FFDFD3", p: "10px" }} position="static">
           <Toolbar>
-            <Typography variant="h4" component="div" sx={{ flexGrow: 1, color: "black"}}>
-            BOOKING PARK
+            <Typography
+              variant="h4"
+              component="div"
+              sx={{ flexGrow: 1, color: "black" }}
+            >
+              BOOKING PARK
             </Typography>
-          
-              <Button
-                data-testid="button-logout"
-                style={{
-                  textDecoration: "none",
-                  backgroundColor: "red ",
-                  padding: "8px 12px",
-                  fontWeight: "bold",
-                  color: "#FFDFD3",
-                }}
-                // onClick={onLogout}
-              >
-                Logout
-              </Button>
-      
+
+            <Button
+              data-testid="button-logout"
+              style={{
+                textDecoration: "none",
+                backgroundColor: "red ",
+                padding: "8px 12px",
+                fontWeight: "bold",
+                color: "#FFDFD3",
+              }}
+              onClick={onLogout}
+            >
+              Logout
+            </Button>
           </Toolbar>
         </AppBar>
       </Box>
